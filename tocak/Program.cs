9a9eea8c5daa1201129216ae4d9269f8203d373d
usern4mehere9 <91462628+usern4mehere9@users.noreply.github.com>
2021-10-06 12:56:27 +0200

    Add files via upload

---
+++ tocak/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace tocak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n, v, t, s, br, o;
+            n = double.Parse(Console.ReadLine());
+            v = double.Parse(Console.ReadLine());
+            t = double.Parse(Console.ReadLine());
+
+            s = v * t;
+            n = n / 100;
+            o = n * Math.PI;
+
+            br = Math.Ceiling(s / o);
+
+            Console.WriteLine(br);
+        }
+    }
+}
+
